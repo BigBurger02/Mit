81a6f9e0e4864aec68771b0dba765a3ac597c409
Vladislav Polovoy <vlad.polovoy@gmail.com>
2024-10-26 18:59:57 +0300

    Init-2

--- MIT_1/Migrations/20241026155919_Init-5.Designer.cs
+++ MIT_1/Migrations/20241026155919_Init-5.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MIT_1.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MIT_1.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241026155919_Init-5")]
+    partial class Init5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
---
+++ MIT_1/Migrations/20241026155919_Init-5.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MIT_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_Plans_PlanId",
+                table: "Invoices");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Plans_PlanId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Plans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_PlanId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_PlanId",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "PlanId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PlanId",
+                table: "Invoices");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Limit",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Limit",
+                table: "Invoices",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Limit",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Limit",
+                table: "Invoices");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlanId",
+                table: "Users",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlanId",
+                table: "Invoices",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Plans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plans", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_PlanId",
+                table: "Users",
+                column: "PlanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_PlanId",
+                table: "Invoices",
+                column: "PlanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_Plans_PlanId",
+                table: "Invoices",
+                column: "PlanId",
+                principalTable: "Plans",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Plans_PlanId",
+                table: "Users",
+                column: "PlanId",
+                principalTable: "Plans",
+                principalColumn: "Id");
+        }
+    }
+}
